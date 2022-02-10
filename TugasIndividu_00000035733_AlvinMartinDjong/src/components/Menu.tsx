d939ded86df49dc9e99f2ab28303bda402b770bf
AlvinMD <alvinmartindwrk@gmail.com>
2022-02-10 16:44:34 +0700

    side menu styles

--- TugasIndividu_00000035733_AlvinMartinDjong/src/components/Menu.tsx
+++ TugasIndividu_00000035733_AlvinMartinDjong/src/components/Menu.tsx
@@ -45,7 +45,7 @@ const Menu: React.FC = () => {
   const location = useLocation();
 
   return (
-    <IonMenu contentId="main" type="overlay">
+    <IonMenu contentId="main" type="push">
       <IonContent>
         <IonList id="item-list">
           {appPages.map((appPage, index) => {
