fd282b365a16558ea97e36ecd28065ce11429e89
Alvin <alvinmartindwrk@gmail.com>
2022-03-02 13:10:35 +0700

    fix tab active appears on spam page

--- Week06_00000035733_AlvinMartinDjong/src/pages/MailTabs.tsx
+++ Week06_00000035733_AlvinMartinDjong/src/pages/MailTabs.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react'
 import { mailOutline, videocamOutline } from 'ionicons/icons'
-import { Redirect, Route } from 'react-router'
+import { Redirect, Route, useLocation } from 'react-router'
 
 import Mail from './Mail'
 import Meet from './Meet'
 import Spam from './Spam'
 
 const MailTabs: React.FC = () => {
+  const { pathname } = useLocation()
+  
   return (
     <IonTabs>
       <IonRouterOutlet>
@@ -17,11 +19,11 @@ const MailTabs: React.FC = () => {
         <Route exact path="/tabs/spam" component={Spam} />
       </IonRouterOutlet>
       <IonTabBar slot='bottom'>
-        <IonTabButton tab='mail' href='/tabs/mail'>
+        <IonTabButton selected={pathname.includes('mail')} tab='mail' href='/tabs/mail'>
           <IonIcon icon={mailOutline} />
           <IonLabel>Mail</IonLabel>
         </IonTabButton>
-        <IonTabButton tab='meet' href='/tabs/meet'>
+        <IonTabButton selected={pathname.includes('meet')} tab='meet' href='/tabs/meet'>
           <IonIcon icon={videocamOutline} />
           <IonLabel>Meet</IonLabel>
         </IonTabButton>
