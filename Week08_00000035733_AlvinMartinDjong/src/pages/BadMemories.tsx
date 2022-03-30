f8375532c568840183ff1f8f09a58c7fc36b3c86
Alvin <alvinmartindwrk@gmail.com>
2022-03-30 21:23:44 +0700

    create view for bad memories

--- Week08_00000035733_AlvinMartinDjong/src/pages/BadMemories.tsx
+++ Week08_00000035733_AlvinMartinDjong/src/pages/BadMemories.tsx
@@ -3,18 +3,28 @@ import {
   IonApp,
   IonButton,
   IonButtons,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCol,
   IonContent,
   IonFab,
   IonFabButton,
+  IonGrid,
   IonHeader,
   IonIcon,
+  IonRow,
   IonTitle,
   IonToolbar
 } from '@ionic/react';
 import { addOutline } from 'ionicons/icons';
-import React from 'react';
+import React, { useContext } from 'react';
+import MemoriesContext from '../data/memories-context';
 
 const BadMemories: React.FC = () => {
+  const memoriesCtx = useContext(MemoriesContext);
+  const badMemories = memoriesCtx.memories.filter(memory => memory.type === 'bad');
+
   return (
     <IonApp>
       <IonHeader>
@@ -30,7 +40,27 @@ const BadMemories: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
-        <h2>Bad Memories</h2>
+        <IonGrid>
+          {badMemories.length === 0 && (
+            <IonRow>
+              <IonCol className='ion-text-center'>
+                <h2>No bad memories found.</h2>
+              </IonCol>
+            </IonRow>
+          )}
+          {badMemories.map(memory => (
+            <IonRow key={memory.id}>
+              <IonCol>
+                <IonCard>
+                  <img src={memory.base64Url} alt={memory.title} />
+                  <IonCardHeader>
+                    <IonCardTitle>{memory.title}</IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          ))}
+        </IonGrid>
         {isPlatform('android') && (
           <IonFab horizontal='end' vertical='bottom' slot='fixed'>
             <IonFabButton color='primary' routerLink='/tabs/new'>
