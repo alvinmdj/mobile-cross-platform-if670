6cd099cad3bec3b2803b149d34abb29c7c1b7370
Alvin <alvinmartindwrk@gmail.com>
2022-03-30 15:46:17 +0700

    install @capacitor/camera and use camera functionality inside NewMemory.tsx in takePhotoHandler function

--- Week08_00000035733_AlvinMartinDjong/src/pages/NewMemory.tsx
+++ Week08_00000035733_AlvinMartinDjong/src/pages/NewMemory.tsx
@@ -1,10 +1,21 @@
 import { IonApp, IonBackButton, IonButton, IonButtons, IonCol, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRow, IonTitle, IonToolbar } from '@ionic/react'
 import { camera } from 'ionicons/icons'
 import React from 'react'
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera'
 
 import './NewMemory.css'
 
 const NewMemory: React.FC = () => {
+  const takePhotoHandler = async () => {
+    const image = Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 80,
+      width: 500,
+    })
+    console.log(image);
+  }
+
   return (
     <IonApp>
       <IonHeader>
@@ -25,7 +36,7 @@ const NewMemory: React.FC = () => {
             <div className="image-preview">
               <h3>No photo choosen.</h3>
             </div>
-            <IonButton fill='clear'>
+            <IonButton fill='clear' onClick={takePhotoHandler}>
               <IonIcon slot='start' icon={camera} />
               <IonLabel>Take Photo</IonLabel>
             </IonButton>
