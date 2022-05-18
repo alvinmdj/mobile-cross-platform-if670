067b91298233a8e0c9276708da51026dcc93e0e4
Alvin <alvinmartindwrk@gmail.com>
2022-05-18 11:29:37 +0700

    android build & app config (name, icon, splash screen)

--- Week11_00000035733_AlvinMartinDjong/capacitor.config.ts
+++ Week11_00000035733_AlvinMartinDjong/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.alvinmdj.ionic.memories',
-  appName: 'Week11_00000035733_AlvinMartinDjong',
+  appName: 'Ionic Memories - Alvin Martin Djong',
   webDir: 'build',
   bundledWebRuntime: false
 };
